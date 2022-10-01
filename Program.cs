070667150381dff87c1b20b3db1244ac81af737b
Hana <113710023+Hana3417@users.noreply.github.com>
2022-10-01 16:27:31 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+char caracter;
+
+do
+{
+    Console.Write("Ingrese una letra: ");
+    caracter = char.Parse(Console.ReadLine());
+}while (caracter != 'a' && caracter != 'e' && caracter != 'i' && caracter != 'o' && caracter != 'u' && caracter != 'A' && caracter != 'E' && caracter != 'I' && caracter != 'O' && caracter != 'U');
+
+Console.WriteLine("La primera vocal ingresada es " + caracter);
+Console.ReadKey();
