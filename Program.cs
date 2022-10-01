0788df6c7f78e36023bd2f553856c1c2f6a23562
Hana <113710023+Hana3417@users.noreply.github.com>
2022-10-01 16:20:11 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+//Suma de todos los comprendidos de x a y --> suma enre 5 a 9 = 5+6+7+8+9
+
+int num1, num2, suma = 0;
+
+Console.WriteLine("Ingrese el primer número");
+num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo número");
+num2 = int.Parse(Console.ReadLine());
+
+for (int i = num1; i <= num2; i++)
+{
+    suma = suma + i;
+}
+
+Console.WriteLine("La suma comprendida entre estos dos números es: : " + suma);
+
+Console.ReadKey();
