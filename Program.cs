ed32e883b4926fd7c064a3408e582e1019bccfc3
Hana <113710023+Hana3417@users.noreply.github.com>
2022-10-01 16:26:33 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int num, suma = 0;
+
+Console.WriteLine("Escriba el número máximo:");
+num = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= num; i = i + 2)
+{
+    suma = suma + i;
+}
+
+Console.WriteLine($"La suma de los primeros números naturales impares menores o iguales que {num} es {suma}");
+
+Console.ReadKey();
