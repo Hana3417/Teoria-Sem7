ed3c88fc896ff083c4e97523ecdb0911922620aa
Hana <113710023+Hana3417@users.noreply.github.com>
2022-10-01 16:17:45 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+int x, y, altura = 5;
+
+for (x = 1; x <= altura; x++)
+{
+    for (y = altura - x; y > 0; y--)
+    {
+        Console.Write(" ");
+    }
+    for (y = 1; y < x; y++)
+    {
+        Console.Write("**");
+    }
+    Console.WriteLine("*");
+}
+
+Console.ReadKey();
